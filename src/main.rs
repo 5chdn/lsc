ffd108bd76713a2eeca8a206c284afe12e668704
5chdn <5chdn@users.noreply.github.com>
2017-04-14 20:38:24 +0200

    Quit on q key

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,20 @@ fn main() {
   // custom tty style sheet
   let _ = siv.load_theme_file("assets/style.toml");
 
+  // quit on q key press
+  siv.add_global_callback('q', |s| s.quit());
+
   // add hello-world dialog with quit button
   siv.add_layer(
     Dialog::around(
-      TextView::new("hello, world!1")
+      TextView::new(
+"182. it will be objected that the french and russian revolutions were
+     failures. but most revolutions have two goals. one is to destroy an
+     old form of society and the other is to set up the new form of society
+     envisioned by the revolutionaries. the french and russian
+     revolutionaries failed (fortunately!) to create the new kind of
+     society of which they dreamed, but they were quite successful in
+     destroying the existing form of society.")
     ).title("lsc").button("ok", |s| s.quit())
   );
 
